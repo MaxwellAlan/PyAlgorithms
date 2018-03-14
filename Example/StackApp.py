e8488c86691ae556664b909e97808f89cbfbb97a
MaxwellAlan <alanmaxwell@163.com>
2018-03-14 21:38:19 +0800

    add stack application - decimal conversion

--- Example/StackApp.py
+++ Example/StackApp.py
@@ -48,3 +48,37 @@ def match(symbol1,symbol2):
     open='([{'
     close=')]}'
     return open.index(symbol1) == close.index(symbol2)
+
+
+#十进制转二进制
+def divideBy2(decNumber):
+    s=Stack()
+    temp=decNumber
+    while temp!=0:
+        s.push(temp%2)
+        temp=temp//2
+    binNum=''
+    while not s.is_empty():
+        binNum=binNum+str(s.pop())
+
+    return binNum
+
+#扩展：除任意基数
+def baseConverter(decNumber,base):
+    s=Stack()
+    digits="0123456789ABCDEF"
+
+    temp=decNumber
+    while temp>0:
+        s.push(temp%base)
+        temp=temp//base
+    baseNum=''
+    while not s.is_empty():
+        baseNum+=digits[s.pop()]
+    return baseNum
+
+
+
+if __name__ == '__main__':
+    print(divideBy2(233))
+    print(baseConverter(25,16))
