41b3019bd1194c451bb33efb62545e155a0103a0
MaxwellAlan <alanmaxwell@163.com>
2018-03-14 21:11:55 +0800

    add stack application - Matching Parentheses complex

--- Example/StackApp.py
+++ Example/StackApp.py
@@ -1,6 +1,6 @@
 from DataStructure.PyStack import Stack
 
-#括号匹配
+#简单括号匹配
 def parMatch(inputString):
     index=0
     s=Stack()
@@ -21,3 +21,30 @@ def parMatch(inputString):
         return False
 
 # print(parMatch('(()()()()'))
+
+def comParMatch(inputString):
+    s=Stack()
+    index=0
+    balance=True
+    while index<len(inputString) and balance:
+        symbol=inputString[index]
+        if symbol in '([{':
+            s.push(symbol)
+        else:
+            if s.is_empty():
+                balance=False
+            else:
+                symbol2=s.pop()
+                if not match(symbol,symbol2):
+                    balance=False
+        index +=1
+    if balance and s.is_empty():
+        return True
+    else:
+        return False
+
+#比较下标是否相等
+def match(symbol1,symbol2):
+    open='([{'
+    close=')]}'
+    return open.index(symbol1) == close.index(symbol2)
