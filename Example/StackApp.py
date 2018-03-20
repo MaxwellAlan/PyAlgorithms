038528dedfe7526dd53b15dacb0afe8d2aff9727
MaxwellAlan <alanmaxwell@163.com>
2018-03-20 22:18:50 +0800

    add infix to postfix expression

--- Example/StackApp.py
+++ Example/StackApp.py
@@ -77,8 +77,34 @@ def baseConverter(decNumber,base):
         baseNum+=digits[s.pop()]
     return baseNum
 
+#中缀表达式转后缀表达式
+def infixToPostfix(infixexpr):
+    prec={"*":3,"/":3,"+":2,"-":2,"(":1}
+    opStack=Stack()
+    postfixList=[]
+    tokenList=infixexpr.split()
 
+    for token in tokenList:
+        if token in "ABCDEFGHIJKLMNOPQRSTUVWXYZ" or token in "0123456789":
+            postfixList.append(token)
+        elif token=='(':
+            opStack.push(token)
+        elif token==')':
+            topToken=opStack.pop()
+            while topToken!='(':
+                postfixList.append(topToken)
+                topToken=opStack.pop()
+        else:
+            while(not opStack.is_empty()) and (prec[opStack.peek()]>= prec[token]):
+                postfixList.append(opStack.pop())
+            opStack.push(token)
+    while not opStack.is_empty():
+        postfixList.append(opStack.pop())
+    return " ".join(postfixList)
 
 if __name__ == '__main__':
-    print(divideBy2(233))
-    print(baseConverter(25,16))
+    # print(divideBy2(233))
+    # print(baseConverter(25,16))
+
+    print(infixToPostfix("A + B * C - ( A "))
+
