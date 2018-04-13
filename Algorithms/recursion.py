773984b47a2f15eff70c616fb8933b5ebe7efd0b
MaxwellAlan <alanmaxwell@163.com>
2018-04-13 13:59:51 +0800

    add dynamic programming

--- Algorithms/recursion.py
+++ Algorithms/recursion.py
@@ -30,6 +30,37 @@ def moveDisk(fp,tp):
     print("moving disk from",fp,"to",tp)
 
 
+# eg4：找零
+def recMC(coinValueList,change):
+   minCoins = change
+   if change in coinValueList:
+     return 1
+   else:
+      for i in [c for c in coinValueList if c <= change]:
+         numCoins = 1 + recMC(coinValueList,change-i)
+         if numCoins < minCoins:
+            minCoins = numCoins
+   return minCoins
+
+def recDC(coinValueList,change,knownResults):
+   minCoins = change
+   if change in coinValueList:
+      knownResults[change] = 1
+      return 1
+   elif knownResults[change] > 0:
+      return knownResults[change]
+   else:
+       for i in [c for c in coinValueList if c <= change]:
+         numCoins = 1 + recDC(coinValueList, change-i,
+                              knownResults)
+         if numCoins < minCoins:
+            minCoins = numCoins
+            knownResults[change] = minCoins
+   return minCoins
+
+
 if __name__ == '__main__':
     # print(toStr(10,2))
-    moveTower(4,"A","B","C")
+    # moveTower(3,"A","B","C")
+    # print(recMC([1,5,10,25],63))
+    print(recDC([1,5,10,25],63,[0]*64))
