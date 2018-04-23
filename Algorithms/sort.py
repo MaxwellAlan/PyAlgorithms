193f6e2fb245f17f73030104df8da4eb24de23fe
AlanMaxwell <alanmaxwell@163.com>
2018-04-23 15:22:04 +0800

    add quick sort

--- Algorithms/sort.py
+++ Algorithms/sort.py
@@ -80,6 +80,43 @@ def mergeSort(alist):
             j += 1
             k += 1
         print("merging",alist)
+
+def quickSort(alist):
+    quickSortHelper(alist,0,len(alist)-1)
+
+def quickSortHelper(alist,first,last):
+    if first < last:
+        splitpoint = partition(alist,first,last)
+        quickSortHelper(alist,first,splitpoint-1)
+        quickSortHelper(alist,splitpoint+1,last)
+
+def partition(alist,first,last):
+    pivotvalue = alist[first]
+
+    leftmark = first + 1
+    rightmark = last
+
+    done = False
+
+    while not done:
+        while leftmark <= rightmark and alist[leftmark] <= pivotvalue:
+            leftmark += 1
+        while alist[rightmark] >=pivotvalue and rightmark >= leftmark:
+            rightmark -= 1
+        if rightmark < leftmark:
+            done = True
+        else:
+            temp = alist[leftmark]
+            alist[leftmark] = alist[rightmark]
+            alist[rightmark] = temp
+    temp = alist[first]
+    alist[first] = alist[rightmark]
+    alist[rightmark] = temp
+
+    return rightmark
+
+
+
 def bubbleSortTest():
     alist = [54, 26, 93, 17, 77, 31, 44, 55, 20]
     bubbleSort(alist)
@@ -110,10 +147,16 @@ def mergeSortTest():
     mergeSort(alist)
     print(alist)
 
+def quickSortTest():
+    alist = [54, 26, 93, 17, 77, 31, 44, 55, 20]
+    quickSort(alist)
+    print(alist)
+
 if __name__ == '__main__':
     # bubbleSortTest()
     # shortBubbleSortTest()
     # selectSortTest()
     # insertSortTest()
     # shellSortTest()
-    mergeSortTest()
+    # mergeSortTest()
+    quickSortTest()
