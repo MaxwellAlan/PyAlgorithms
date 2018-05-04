68e54bcb9a76c1ca239114ecef0821d8a02fcb1d
AlanMaxwell <alanmaxwell@163.com>
2018-05-04 17:31:26 +0800

    add tree application

---
+++ Example/TreeApp.py
@@ -0,0 +1,45 @@
+from DataStructure.PyTree import BinaryTree
+from DataStructure.PyStack import Stack
+import operator
+
+def buildParseTree(fpexp):
+    fplist = fpexp.split(' ')
+    pStack = Stack()
+    eTree = BinaryTree('')
+    pStack.push(eTree)
+    currentTree = eTree
+    for i in fplist:
+        if i == '(':
+            currentTree.insertLeft('')
+            pStack.push(currentTree)
+            currentTree = currentTree.getLeftChild()
+        elif i not in ['+','-','*','/',')']:
+            currentTree.setRootVal(int(i))
+            parent = pStack.pop()
+            currentTree = parent
+        elif i in ['+','-','*','/']:
+            currentTree.setRootVal(i)
+            currentTree.insertRight('')
+            pStack.push(currentTree)
+            currentTree = currentTree.getRightChild()
+        elif i == ')':
+            currentTree = pStack.pop()
+        else:
+            raise ValueError
+    return eTree
+
+def evaluate(parseTree):
+    opers = {'+':operator.add,'-':operator.sub,'*':operator.mul,'/':operator.truediv}
+
+    leftC = parseTree.getLeftChild()
+    rightC = parseTree.getRightChild()
+
+    if leftC and rightC:
+        fn = opers[parseTree.getRootVal()]
+        return fn(evaluate(leftC),evaluate(rightC))
+    else:
+        return parseTree.getRootVal()
+
+if __name__ == '__main__':
+    pt = buildParseTree("( ( 10 + 5 ) * 3 )")
+    print(evaluate(pt))
