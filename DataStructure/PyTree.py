1506b534eb1141c5507b2c4a1440093efc0473ca
AlanMaxwell <alanmaxwell@163.com>
2018-05-08 14:07:18 +0800

    add bin heap

--- DataStructure/PyTree.py
+++ DataStructure/PyTree.py
@@ -59,6 +59,66 @@ def inorder(tree):
         print(tree.getRootVal())
         inorder(tree.getRightChild())
 
+class BinHeap:
+    def __init__(self):
+        self.heaplist = [0]
+        self.currentSize = 0
+
+    # 堆插入
+    # 子结点与父结点交换
+    def percUp(self,i):
+        while i // 2 > 0:
+            if self.heaplist[i] < self.heaplist[i // 2]:
+                tmp = self.heaplist[i]
+                self.heaplist[i] = self.heaplist[i // 2]
+                self.heaplist[i // 2] = tmp
+            i = i // 2
+
+    # 每次插入时在列表最后添加数据，之后进行堆的维护,保证父结点小于子结点
+    def insert(self,k):
+        self.heaplist.append(k)
+        self.currentSize = self.currentSize + 1
+        self.percUp(self.currentSize)
+
+    # 删除元素
+    # 为了维护堆顺序属性，我们所需要做的是将根节点和最小的子节点交换。
+    # 在初始交换之后，我们可以将节点和其子节点重复交换，直到节点被交换到正确的位置，使它小于两个子节点。
+    def percDown(self,i):
+        while (i * 2) <= self.currentSize:
+            mc = self.minChild(i)
+            if self.heaplist[i] > self.heaplist[mc]:
+                tmp = self.heaplist[i]
+                self.heaplist[i] = self.heaplist[mc]
+                self.heaplist[mc] = tmp
+            i = mc
+
+    def minChild(self,i):
+        if i * 2 + 1 > self.currentSize:
+            return  i * 2
+        else:
+            if self.heaplist[i * 2] < self.heaplist[i * 2 + 1]:
+                return i * 2
+            else:
+                return i * 2 + 1
+
+    def delMin(self):
+        retual = self.heaplist[1]
+        self.heaplist[1] = self.heaplist[self.currentSize]
+        self.currentSize = self.currentSize - 1
+        self.heaplist.pop()
+        self.percDown(1)
+        return retual
+
+    # 构建二叉堆
+    def buildHeap(self,alist):
+        i = len(alist) // 2
+        self.currentSize = len(alist)
+        self.heaplist = [0] + alist[:]
+        while(i > 0):
+            self.percDown(i)
+            i = i - 1
+
+
 
 if __name__ == '__main__':
     r = BinaryTree('a')
