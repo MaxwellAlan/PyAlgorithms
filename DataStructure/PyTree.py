23f23b077d669c23e8feaa3a7bc23f7d3a61fca9
AlanMaxwell <alanmaxwell@163.com>
2018-05-09 21:59:39 +0800

    binary tree put and get

--- DataStructure/PyTree.py
+++ DataStructure/PyTree.py
@@ -118,7 +118,121 @@ class BinHeap:
             self.percDown(i)
             i = i - 1
 
+# 二叉查找树
 
+# 树结点
+class TreeNode:
+    def __init__(self,key,val,left=None,right=None,parent=None):
+        self.key = key
+        self.payload = val
+        self.leftChild = left
+        self.rightChild = right
+        self.parent = parent
+
+    def hasLeftChild(self):
+        return self.leftChild
+
+    def hasRightChild(self):
+        return self.rightChild
+
+    # 判断是否为左子树：具有父结点 且 是父结点的左子树
+    def isLeftChild(self):
+        return self.parent and self.parent.leftChild == self
+
+    def isRightChild(self):
+        return self.parent and self.parent.rightChild == self
+
+    def isRoot(self):
+        return not self.parent
+
+    def isLeaf(self):
+        return not (self.rightChild or self.leftChild)
+
+    def hasAnyChildren(self):
+        return self.rightChild or self.leftChild
+
+    def hasBothChildren(self):
+        return self.rightChild and self.leftChild
+
+
+    def replaceNodeData(self,key,value,lc,rc):
+        self.key = key
+        self.payload = value
+        self.leftChild = lc
+        self.rightChild = rc
+        # 替换结点：如果该结点存在子结点，则需将子结点的父结点更换为该结点
+        if self.hasLeftChild():
+            self.leftChild.parent = self
+        if self.hasRightChild():
+            self.rightChild.parent = self
+
+class BinarySearchTree:
+    def __init__(self):
+        self.root = None
+        self.size = 0
+
+    def __len__(self):
+        return self.size
+
+    def __iter__(self):
+        return self.root.__iter__()
+
+    def length(self):
+        return self.size
+
+    # 递归实现，存在bug：重复键会存入右子树，待修复
+    def _put(self,key,val,currentNode):
+        if key < currentNode.key:
+            if currentNode.hasLeftChild():
+                self._put(key,val,currentNode.leftChild)
+            else:
+                currentNode.leftChild = TreeNode(key,val,parent=currentNode)
+        elif key == currentNode.key:
+            currentNode.replaceNodeData(key,val,currentNode.hasLeftChild(),currentNode.hasRightChild())
+        else:
+            if currentNode.hasRightChild():
+                self._put(key,val,currentNode.rightChild)
+            else:
+                currentNode.rightChild = TreeNode(key,val,parent=currentNode)
+
+    def put(self,key,val):
+        if self.root:
+            self._put(key,val,self.root)
+        else:
+            self.root = TreeNode(key,val)
+        self.size = self.size + 1
+
+    def __setitem__(self, key, value):
+        self.put(key,value)
+
+    def get(self,key):
+        if self.root:
+            res = self._get(key,self.root)
+            if res:
+                return res.payload
+            else:
+                return None
+        else:
+            return None
+
+    def _get(self,key,currentNode):
+        if not currentNode:
+            return None
+        elif currentNode.key == key:
+            return currentNode
+        elif key < currentNode.key:
+            return self._get(key,currentNode.leftChild)
+        else:
+            return self._get(key,currentNode.rightChild)
+
+    def __getitem__(self, key):
+        return self.get(key)
+
+    def __contains__(self, key):
+        if self._get(key,self.root):
+            return True
+        else:
+            return False
 
 if __name__ == '__main__':
     r = BinaryTree('a')
